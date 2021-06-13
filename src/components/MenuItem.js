07a2b99a4f173137870a4cbb91afb7be9228902e
nikolina-jovanovic <85742865+nikolina-jovanovic@users.noreply.github.com>
2021-06-13 20:26:32 +0200

    Update MenuItem.js

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -3,16 +3,16 @@ import ItemInfo from "./ItemInfo";
 
 const MenuItem = ({ menuItems, onAdd, onRemove }) => {
   const images = [
-    "../images/1.png",
-    "../images/2.png",
-    "../images/3.png",
-    "../images/4.png",
-    "../images/5.png",
-    "../images/6.png",
-    "../images/7.png",
-    "../images/8.png",
-    "../images/9.png",
-    "../images/10.png"
+    "/1.png",
+    "/images/2.png",
+    "/images/3.png",
+    "/images/4.png",
+    "/images/5.png",
+    "/images/6.png",
+    "/images/7.png",
+    "/images/8.png",
+    "/images/9.png",
+    "/images/10.png"
   ];
 
 
